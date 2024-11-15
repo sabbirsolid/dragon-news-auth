8c916f798757db55a0fe1d3b283685247963e13b
sabbirsolid <sabbir.designer21@gmail.com>
2024-11-15 23:46:09 +0600

    created a project

---
+++ src/routes/router.jsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
+import CategoryNews from "../pages/CategoryNews";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../layouts/Login";
+import Register from "../layouts/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>,
+    children: [
+      {
+        path: "",
+        element: <Navigate to={"/category/01"}></Navigate>,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews></CategoryNews>,
+        loader: ({ params }) =>
+          fetch(
+            `https://openapi.programming-hero.com/api/news/category/${params.id}`
+          ),
+      },
+    ],
+  },
+  {
+    path: "/news",
+    element: <h1>News Layout</h1>,
+  },
+  {
+    path: "auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/auth/register",
+        element: <Register></Register>
+      }
+    ]
+  },
+  {
+    path: "*",
+    element: <h1>Error</h1>,
+  },
+]);
+
+export default router;
